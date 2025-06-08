5d530fdad75a06eb5ea439f4c97b8ad2723a72e7
Aditya Deshpande <Aditya@Radhas-MacBook-Air.local>
2025-06-08 18:04:43 +0530

    Deployer file

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+aws cloudformation deploy \
+  --template-file alb-template.yaml \
+  --stack-name alb-all \
+  --parameter-overrides \
+      Environment=all \
+      VpcId=vpc-5fe4rfg8 \
+      PublicSubnetIds="subnet-1,subnet-2" \
+      PrivateSubnetIds="subnet-3,subnet-4" \
+      SecurityGroups="sg-xyz123"

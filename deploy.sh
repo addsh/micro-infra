8a4811657319d6d585c49db7cbb6e334710d6efc
Aditya Deshpande <Aditya@Radhas-MacBook-Air.local>
2025-06-11 09:49:37 +0530

    Deployer file

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ aws cloudformation deploy \
       VpcId=vpc-5fe4rfg8 \
       PublicSubnetIds="subnet-1,subnet-2" \
       PrivateSubnetIds="subnet-3,subnet-4" \
-      SecurityGroups="sg-xyz123"
+      SecurityGroups="sg-123"
